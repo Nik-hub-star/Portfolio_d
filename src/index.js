7f32bad58a074093eea21c6b6acb9605b4754a45
Nik-hub-star <nikhiljha786@gmail.com>
2020-06-03 01:45:52 +0530

    Lazy loading demo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import "./sass/main.scss";
-console.log("JHeljkhjoo");
+
+document.addEventListener("DOMContentLoaded", function() {
+  let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      });
+    });
+    lazyImages.forEach(element => {
+      lazyImageObserver.observe(element);
+    });
+  }
+});
